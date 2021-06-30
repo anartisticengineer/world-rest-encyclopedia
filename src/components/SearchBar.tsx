e6ffafe036d37351eb5c3bf8f62abdd1d86a2217
Justin Johnson <jj@Justins-MacBook-Pro.local>
2021-06-29 21:10:40 -0600

    Added ref hook to search bar to focus input upon opening

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,18 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useRef } from 'react';
 
 const SearchBar: FunctionComponent<any> = ({ onChangeFcn }) => {
+  const inputEl: any = useRef(null);
+  useEffect(() => {
+    inputEl.current.focus();
+  }, []);
   return (
-    <input className="searchbar" placeholder="Search" onChange={onChangeFcn} />
+    <input
+      className="searchbar"
+      placeholder="Search"
+      onChange={onChangeFcn}
+      ref={inputEl}
+      type="text"
+    />
   );
 };
 
